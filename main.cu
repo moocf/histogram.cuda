4fdfbea23c58459d29416225eeb64fdc76d5e49b
Subhajit Sahu <wolfram77@gmail.com>
2021-12-02 13:52:43 +0530

    :turtle: update src

--- main.cu
+++ main.cu
@@ -1,25 +1,24 @@
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include "support.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include "src/main.hxx"
 
 
 const int threads = 256;
 
-void memset_rand(uint8 *buf, int N) {
+
+void memsetRand(uint8_t *buf, int N) {
   for (int i=0; i<N; i++)
     buf[i] = rand() & 0xFF;
 }
 
-void histogram(int *hist, uint8 *buff, int N) {
+void histogram(int *hist, const uint8_t *buff, int N) {
   for (int i=0; i<N; i++) // 1
     hist[buff[i]]++;      // 1
 }
 
-int histogram_sum(int *hist, int N) {
+int histogramSum(const int *hist, int N) {
   int sum = 0;
   for (int i=0; i<N; i++)
     sum += hist[i];
@@ -30,11 +29,11 @@ int histogram_sum(int *hist, int N) {
 // Each thread atomically increments the bytes in buffer meant for it.
 // This however leads to high contention to the 256 locations in the
 // global memory.
-// 
+//
 // 1. Get byte at buffer for this thread.
 // 2. Atomically increment appropriate index in histogram.
 // 3. Shift to the next byte, by a stride.
-__global__ void kernel(int *hist, uint8 *buff, int N) {
+__global__ void kernel(int *hist, const uint8_t *buff, int N) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
 
   while (i < N) {
@@ -49,7 +48,7 @@ __global__ void kernel(int *hist, uint8 *buff, int N) {
 // buffer is consumed. Then each thread in the block updates the
 // histogram in the global memory atomically. This reduces global
 // memory contention.
-// 
+//
 // 1. Initialize shared memory (of size 256).
 // 2. Get byte at buffer for this thread.
 // 3. Atomically increment appropriate index in shared memory.
@@ -57,11 +56,11 @@ __global__ void kernel(int *hist, uint8 *buff, int N) {
 // 5. Wait for all threads within the block to finish.
 // 5. Reduce the sum in the cache to a single value in binary tree fashion.
 // 6. Atomically update per-block histogram into global histogram.
-__global__ void kernel_shared(int *hist, uint8 *buff, int N) {
+__global__ void kernelShared(int *hist, const uint8_t *buff, int N) {
   __shared__ int temp[threads];
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int t = threadIdx.x;
-  
+
   temp[t] = 0;     // 1
   __syncthreads(); // 1
 
@@ -75,7 +74,7 @@ __global__ void kernel_shared(int *hist, uint8 *buff, int N) {
 }
 
 
-int run_cpu(uint8* buff, int N) {
+int runCpu(const uint8_t* buff, int N) {
   int H = 256;
   int H1 = H * sizeof(int);
 
@@ -85,22 +84,22 @@ int run_cpu(uint8* buff, int N) {
   clock_t begin = clock();
   histogram(hist, buff, N);
   clock_t end = clock();
-  
+
   double duration = (double) (end - begin) / CLOCKS_PER_SEC;
   printf("CPU execution time: %3.1f ms\n", duration * 1000);
-  printf("CPU Histogram sum: %d\n", histogram_sum(hist, H));
+  printf("CPU Histogram sum: %d\n", histogramSum(hist, H));
 
   free(hist);
   return 0;
 }
 
 
-int run_gpu(uint8 *buff, int N, int shared) {
+int runGpu(const uint8_t *buff, int N, int shared) {
   int H = 256;
-  int N1 = N * sizeof(uint8);
+  int N1 = N * sizeof(uint8_t);
   int H1 = H * sizeof(int);
 
-  uint8 *buffD;
+  uint8_t *buffD;
   int *hist, *histD;
   hist = (int*) malloc(H1);
 
@@ -118,7 +117,7 @@ int run_gpu(uint8 *buff, int N, int shared) {
   TRY(cudaGetDeviceProperties(&p, 0));
   int blocks = 2 * p.multiProcessorCount;
   if (!shared) kernel<<<blocks, threads>>>(histD, buffD, N);
-  else kernel_shared<<<blocks, threads>>>(histD, buffD, N);
+  else kernelShared<<<blocks, threads>>>(histD, buffD, N);
 
   float duration;
   TRY( cudaMemcpy(hist, histD, H1, cudaMemcpyDeviceToHost) );
@@ -126,7 +125,7 @@ int run_gpu(uint8 *buff, int N, int shared) {
   TRY( cudaEventSynchronize(stop) );
   TRY( cudaEventElapsedTime(&duration, start, stop) );
   printf("GPU execution time: %3.1f ms\n", duration);
-  printf("GPU Histogram sum: %d\n", histogram_sum(hist, H));
+  printf("GPU Histogram sum: %d\n", histogramSum(hist, H));
 
   int* histH = (int*) malloc(H1);
   memset(histH, 0, H1);
@@ -147,21 +146,21 @@ int run_gpu(uint8 *buff, int N, int shared) {
 
 int main() {
   int N = 1000000;
-  int N1 = N * sizeof(uint8);
+  int N1 = N * sizeof(uint8_t);
 
-  uint8 *buff = (uint8*) malloc(N1);
-  memset_rand(buff, N1);
+  uint8_t *buff = (uint8_t*) malloc(N1);
+  memsetRand(buff, N1);
 
   printf("CPU Histogram ...\n");
-  run_cpu(buff, N);
+  runCpu(buff, N);
   printf("\n");
 
   printf("GPU Histogram: atomic ...\n");
-  run_gpu(buff, N, 0);
+  runGpu(buff, N, 0);
   printf("\n");
 
   printf("GPU Histogram: shared + atomic ...\n");
-  run_gpu(buff, N, 1);
+  runGpu(buff, N, 1);
   printf("\n");
 
   free(buff);
